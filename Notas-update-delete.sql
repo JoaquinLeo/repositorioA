cf7705607f6c57e0fda46766223e52931b9b5624
Joaquin <joaquinleo@gmail.com>
2022-11-05 03:12:49 -0300

    vista agregada

--- Notas-update-delete.sql
+++ Notas-update-delete.sql
@@ -4,3 +4,11 @@ UPDATE alumnos set edad = 24 where alumno_id = 4;
 -- eliminacion en cascada
 DELETE from notas where alumno_id = 11;
 DELETE from alumnos where alumno_id = 11;
+
+create view desaprobados as
+select alumnos.nombre , materias.nombre as nombre_materia , notas.calificacion from alumnos
+inner join notas on notas.alumno_id = alumnos.alumno_id
+inner join materias on materias.materia_id = notas.materia_id
+where estado = 'Desaprobada';
+
+select * from desaprobados;
